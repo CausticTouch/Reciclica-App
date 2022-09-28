f748c6eaad37677363f626b51e11def9075130ed
EduardoDuarte <a20139@alunos.ipca.pt>
2022-09-28 12:03:19 +0100

    day 8 commit 1 (7.5/XX)

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { LoaderPage } from './loader.page';
@@ -6,6 +7,7 @@ import { LoaderPage } from './loader.page';
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -14,11 +16,16 @@ describe('LoaderPage', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoaderPage);
+
+    router = TestBed.get(Router);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    component.ngOnInit();
+    
+    expect(router.navigate).toHaveBeenCalledWith('login');
   });
 });
