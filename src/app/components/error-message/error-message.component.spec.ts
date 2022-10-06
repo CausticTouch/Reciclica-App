5a18e4ed60e0ee13f68876eeb26f5f13cef05ea9
EduardoDuarte <a20139@alunos.ipca.pt>
2022-10-06 14:56:24 +0100

    day 13 commit(9.8)
    11:10

---
+++ src/app/components/error-message/error-message.component.spec.ts
@@ -0,0 +1,41 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormControl, FormGroup } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { ErrorMessageComponent } from './error-message.component';
+
+describe('ErrorMessageComponent', () => {
+  let component: ErrorMessageComponent;
+  let fixture: ComponentFixture<ErrorMessageComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ErrorMessageComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ErrorMessageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it ('show error message', () => {
+    component.field = new FormGroup({anyField: new FormControl()});
+
+    component.field.markAsTouched();
+    component.field.setErrors({anyError: true});
+    component.error = "anyError"
+
+    expect(component.shouldShowComponent()).toBeTruthy();
+  });
+
+  it ('show error message2', () => {
+    component.field = new FormGroup({anyField: new FormControl()});
+
+    component.field.setErrors({anyError: true});
+    component.error = "anyError"
+
+    expect(component.shouldShowComponent()).toBeFalsy();
+  });
+
+});
